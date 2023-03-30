c2bd0f22665af23f35eed5bd6b6a92fd997d87a1
Desiree Martinez <des@Desirees-MacBook-Air.local>
2023-03-30 16:52:01 -0400

    pushing file

---
+++ src/com/promineotech/Application.java
@@ -0,0 +1,23 @@
+package com.promineotech;
+
+public class Application {
+
+	//Entry point for Java program
+	public static void main(String[] args) {
+		
+		// You write code between curly braces {}
+		
+		// Single line comment
+		
+		/*
+		 * Multi-line comment
+		 */ 
+
+		
+		System.out.println("My first Java Program!");
+		
+		
+		
+	}
+
+}
